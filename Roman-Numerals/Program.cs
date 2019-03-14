d445e22de93e897a4b0c6549f36900b7c1fea66b
Sash <43886774+LowHouse78@users.noreply.github.com>
2019-03-14 12:37:18 +0100

    Initial check in

---
+++ Roman-Numerals/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_Numerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var literals = new List<string>()
+            {
+                "I",
+                "II",
+                "III",
+                "IV",
+                "VIII",
+                "XLII",
+                "MMXIII",
+                "MXI",
+                "MCDXCIX",
+                "MMXXII",
+                "V",
+                "VI",
+                "CX",
+                "CCCLXXV",
+                "MD",
+                "MDLXXV",
+                "MDCL",
+                "MDCCXXV",
+                "MDCCC",
+                "MDCCCLXXV",
+                "MCML",
+                "MMXXV",
+                "MMC",
+                "MMCLXXV",
+                "MMCCL",
+                "MMCCCXXV",
+                "MMCD",
+                "MMCDLXXV",
+                "MMDL",
+                "MMMMMMMMIV",
+                "MMMMMMMM"
+            };
+
+            foreach (var item in literals)
+            {
+                var translator = new RomanNumerals();
+                Console.WriteLine(item.PadRight(12,' ')+"= "+translator.TranslateRomanNumeral(item));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RomanNumerals.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RomanNumerals.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RomanNumerals.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e47a0c3e-8619-45f8-be2e-65f0f5532a94")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
